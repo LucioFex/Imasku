988350f5a260e2f7279f3b3928c6737f78c20d08
LucioFex <Luciano1d2Esteban@gmail.com>
2022-07-07 13:54:49 -0300

    feat: Modification in the data collection for the images sizes of the 'composite' command

--- commands/composite.js
+++ commands/composite.js
@@ -4,7 +4,9 @@ module.exports = {
     async execute(client, message, args) {
         // Save of the image inside an array to add only one image by 'filter'
         const rawImages = []; // Images submited by the user
-        message.attachments.every((file) => rawImages.push(file.url));
+        message.attachments.every((file) => rawImages.push({
+            url: file.url, width: file.width, height: file.height,
+        }));
 
         // In case the user didn't submit an image
         if (rawImages.length < 2) {
@@ -17,23 +19,22 @@ module.exports = {
         const gotModule = await import('got');
         const got = gotModule.default;
 
-        // Getting first image via url
-        const imageUrl = rawImages[0]; // Img 1
+        // Getting first image via url (frontal)
         const sharpStream = sharp();
-        got.stream(imageUrl).pipe(sharpStream);
+        got.stream(rawImages[0].url).pipe(sharpStream); // Img 1
 
-        // Gettings background image via url
-        const bgImageUrl = rawImages[1]; // Img 2
+        // Gettings second image via url (background)
         let bgSharpStream = sharp();
-        got.stream(bgImageUrl).pipe(bgSharpStream);
+        got.stream(rawImages[1].url).pipe(bgSharpStream); // Img 2
+        bgSharpStream = await bgSharpStream.toBuffer(); // THE SOURCE OF ALL THE PROBLEMS?
 
         // Applying effect, and saving it
-        const mainImgSize = await sharpStream.metadata();
-        bgSharpStream.resize(mainImgSize.width, mainImgSize.height);
-        bgSharpStream = await bgSharpStream.toBuffer()
-
-        sharpStream.composite([{ input: bgSharpStream, gravity: 'north', blend: 'dest-over'}]);
-
+        sharpStream.resize(
+            rawImages[1].width,
+            rawImages[1].height,
+            { fit: 'contain', background: '#FFFFFF00' },
+        );
+        sharpStream.composite([{ input: bgSharpStream, gravity: 'north', blend: 'dest-over' }]);
 
         const { randomToken, removeProcessedImage } = require('../helpers/commonFunctions');
         const imageDir = `${__dirname}/src/${randomToken()}.png`;
