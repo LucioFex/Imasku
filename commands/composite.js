8bf523c9b6864b80088ff3a654c094ba48881daa
LucioFex <luciano1d2esteban@gmail.com>
2022-07-11 17:06:28 -0300

    feat: Add of a new logic for the 'composite' command
    extra: It's still not complete

--- commands/composite.js
+++ commands/composite.js
@@ -10,8 +10,17 @@ module.exports = {
 
         // In case the user didn't submit an image
         if (rawImages.length < 2) {
-            return message.channel.send('You need to send two images, the render and the background');
+            return message.channel.send(
+                'You need to send two images, the render and the background',
+            );
         }
+        const {
+            randomToken,
+            removeProcessedImage,
+        } = require('../helpers/commonFunctions');
+
+        const token = randomToken();
+        const strmDir = `${__dirname}/src/strm-${token}.png`;
 
         // Import of packages to process the image
         const sharp = require('sharp');
@@ -20,28 +29,31 @@ module.exports = {
         const got = gotModule.default;
 
         // Getting first image via url (frontal)
-        const sharpStream = sharp();
-        got.stream(rawImages[0].url).pipe(sharpStream); // Img 1
-
-        // Gettings second image via url (background)
-        let bgSharpStream = sharp();
-        got.stream(rawImages[1].url).pipe(bgSharpStream); // Img 2
-        bgSharpStream = await bgSharpStream.toBuffer(); // THE SOURCE OF ALL THE PROBLEMS?
-
-        // Applying effect, and saving it
-        sharpStream.resize(
+        const sharpStream = sharp().resize(
             rawImages[1].width,
             rawImages[1].height,
             { fit: 'contain', background: '#FFFFFF00' },
         );
-        sharpStream.composite([{ input: bgSharpStream, gravity: 'north', blend: 'dest-over' }]);
+        got.stream(rawImages[0].url).pipe(sharpStream); // Img 1
 
-        const { randomToken, removeProcessedImage } = require('../helpers/commonFunctions');
-        const imageDir = `${__dirname}/src/${randomToken()}.png`;
+        // Gettings second image via url (background)
+        const composited = sharp();
+        got.stream(rawImages[1].url).pipe(composited); // Img 2
+        composited.toFile(strmDir); // strm = stream
+
+        // Applying effect, and saving it
+        sharp(await sharpStream.toBuffer())
+            .composite([{
+                input: strmDir, // (?) Keep checking this later...
+                gravity: 'north',
+                blend: 'dest-over',
+            }]);
+
+        const imageDir = `${__dirname}/src/${token}.png`;
         await sharpStream.toFile(imageDir);
 
         // Bot sending the image to the chat
         await message.channel.send({ files: [imageDir] });
-        removeProcessedImage(imageDir);
+        removeProcessedImage(imageDir, strmDir);
     },
 };
--- helpers/commonFunctions.js
+++ helpers/commonFunctions.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const randomToken = () => Math.random().toString(36).substring(2);
 
 // It Removes the 'processedTOKEN.png' file from the 'commands/src' folder
-const removeProcessedImage = (dir) => fs.unlinkSync(dir);
+const removeProcessedImage = (dir1, dir2) => {
+    fs.unlinkSync(dir1);
+    fs.unlinkSync(dir2);
+};
 
 module.exports = {
     randomToken, removeProcessedImage,
