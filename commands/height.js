da5cddd0753c54cf0e49e892948ea8afb452c3c3
LucioFex <luciano1d2esteban@gmail.com>
2022-10-14 21:18:30 -0300

    fix: Of the 'validateResolution' helper

--- commands/height.js
+++ commands/height.js
@@ -4,21 +4,25 @@ module.exports = {
     async execute(client, message, args) {
         // Save of the image inside an array to add only one image by 'filter'
         // Mentions or Images submited by the user
-        const rawImages = [];
+        const rawImages = []; // Images submited by the user
+
         const mention = message.mentions.users.first();
-        message.attachments.find((file) => rawImages.push(file.url));
+        message.attachments.every((file) => rawImages.push({
+            url: file.url, width: file.width, height: file.height,
+        }));
 
         if (mention !== undefined) {
             // If the user mentioned someone, that person's avatar will be edited
-            rawImages.push(mention.avatarURL(), 'avatar');
+            rawImages.push({ url: mention.avatarURL(), width: 256, height: 256 }, 'avatar');
         } else if (rawImages.length === 0) {
             // In case the user has not sent an image or an avatar
-            rawImages.push(message.author.avatarURL(), 'avatar');
+            rawImages.push({ url: message.author.avatarURL(), width: 256, height: 256 }, 'avatar');
         }
 
         // Check if user submitted wrong resolution
         const { validateResolution } = require('../helpers/commonFunctions');
-        const validation = validateResolution(args[0]); // args[0] is the height
+
+        const validation = validateResolution(null, args[0]); // args[0] is the height
         if (validation !== 'correct') return message.channel.send(validation);
 
         // Import of packages to process the image
@@ -27,24 +31,30 @@ module.exports = {
         const gotModule = await import('got');
         const got = gotModule.default;
 
-        // Getting image via url
-        const imageUrl = rawImages[0];
-        const sharpStream = sharp();
-        got.stream(imageUrl).pipe(sharpStream);
+        try {
+            // Images fetch
+            const imageUrl = rawImages[0].url;
+            const imgBuffer = (await got(imageUrl, { responseType: 'buffer' })).body;
 
-        // If the image is an avatar, the size will double (128px * 2) x (128px * 2)
-        if (rawImages[rawImages.length - 1] === 'avatar') sharpStream.resize(256, 256);
+            // Applying effect, and saving it
+            let mode = args[2];
+            if (!['cover', 'inside', 'fill'].includes(args[2])) {
+                mode = 'fill'; // Default fit
+            }
 
-        // Applying resizing, and saving it
-        let mode = args[2];
-        if (!['cover', 'inside', 'fill'].includes(args[2])) {
-            mode = 'fill'; // Default fit
+            // If the image is an avatar, the size will double (128px * 2) x (128px * 2)
+            const sharpStream = sharp(imgBuffer);
+            if (rawImages[rawImages.length - 1] === 'avatar') sharpStream.resize(256, 256);
+
+            // Default width (null), selected height (args[0])
+            sharpStream.resize(rawImages[0].width, parseInt(args[0]), { fit: mode });
+
+            // Bot sending the image to the chat
+            await message.channel.send({ files: [await sharpStream.toBuffer()] });
+        } catch (err) {
+            return message.channel.send(
+                await message.channel.send('I had a problem trying to edit the image 💀'),
+            );
         }
-
-        // Default width (null), selected height (args[0])
-        sharpStream.resize(null, parseInt(args[0]), { fit: mode });
-
-        // Bot sending the image to the chat
-        await message.channel.send({ files: [await sharpStream.toBuffer()] });
     },
 };
--- helpers/commonFunctions.js
+++ helpers/commonFunctions.js
@@ -1,29 +1,24 @@
-const validateResolution = (dim1, dim2 = null) => {
+const validateResolution = (dim1, dim2) => { // Needs refactor with Yup
     /*
     Returns 'correct' if the given width and/or height are a
     'number' (not NaN), greater than 0 and less than or equal to 4000
     */
-    let message = '';
-    dim1 = (dim1 !== null && dim1 !== undefined) ? parseInt(dim1) : null;
-    dim2 = (dim2 !== null && dim2 !== undefined) ? parseInt(dim2) : null;
 
-    const tmp = [dim1, dim2]; // Temporal array constant for iteration
     // Check if both dimensions are between 1px and 4000px
+    const tmp = [parseInt(dim1), parseInt(dim2)];
     for (let idx = 0; idx < tmp.length; idx += 1) {
-        if (tmp[idx] !== null && tmp[idx] > 0 && tmp[idx] <= 4000) {
-            message = 'correct';
-        } else if (tmp[idx] !== null && (tmp[idx] <= 0 || tmp[idx] > 4000)) {
-            message = (
+        if (tmp[idx] > 0 && tmp[idx] <= 4000) {
+            return 'correct';
+        } if (tmp[idx] <= 0 || tmp[idx] > 4000) {
+            return (
                 'Sorry, but each resolution must be between 1 and 4000'
                 + '\nIf you have any questions, check the `resizing` command for more information'
             );
-            break;
-        } else if (tmp[idx] !== null && isNaN(tmp[idx])) {
-            message = 'The resolution you sent is not a number, please try again 👺';
-            break;
         }
     }
-    return message;
+
+    // In case both dimensions are 'NaN'
+    return 'You need to send a resolution, please try again  👀';
 };
 
-module.exports = validateResolution;
+module.exports = { validateResolution };
